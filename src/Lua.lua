61e466e5c752b99a03b73e878eb8462b650079b9
Haruue Icymoon <i@haruue.moe>
2022-05-18 11:18:00 +0800

    docs: minor comment changes for lua

--- src/Lua.lua
+++ src/Lua.lua
@@ -2,10 +2,11 @@
 
 --[[
 the array index of Lua starts from 1, but in fact we can use 0 or even a
-negative index for arg, and the arg[0] just acts like argv[0] in c.
+negative index for arg[], and the arg[0] actually acts like argv[0] in c.
 --]]
 
--- however, the generic for always starts with index=1, so we use numeric for.
+-- however, the generic-for always starts with index=1, so we use the
+-- numeric-for here.
 for i=0,#arg do
     print(string.format("argv[%d]=%s", i, arg[i]))
 end
