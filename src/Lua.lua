06e2126a20ec7ca44d981c6f51ce3d2a1ef1259e
Haruue Icymoon <i@haruue.moe>
2022-05-18 10:53:35 +0800

    fix: argv[0] in lua

--- src/Lua.lua
+++ src/Lua.lua
@@ -1,7 +1,8 @@
 #!/usr/bin/env lua
 
--- there is no argv[0] in lua but we leave it empty
-table.insert(arg, 1, "")
+-- there is arg[0] in lua but won't appear in foreach
+table.insert(arg, 1, arg[0])
+
 for i, v in ipairs(arg) do
     -- lua index starts from 1
     i = i - 1
