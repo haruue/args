717c5a059739e9626cce32a43101607f1655816d
谭九鼎 <109224573@qq.com>
2024-10-18 10:33:09 +0800

    Create CSharp.cs (#2)
    * Create CSharp.cs
    
    * Update CSharp.cs

---
+++ src/CSharp.cs
@@ -0,0 +1,14 @@
+using System;
+
+class CSharp
+{
+    static void Main(string[] args)
+    {
+        string[] argv = new string[args.Length + 1];
+        // see Java.java
+        argv[0] = "";
+        Array.Copy(args, argv, args.Length);
+        for (int i = 0; i < argv.Length; i++)
+            Console.WriteLine("argv[{0}]={1}", i, argv[i]);
+    }
+}
